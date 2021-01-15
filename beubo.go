e09abe4380f7bdd205c12fac3ea461c8c678b263
uberswe <markus@tenghamn.com>
2021-01-15 02:47:01 +0100

    Added prompts to download default theme if it's not present

--- beubo.go
+++ beubo.go
@@ -1,7 +1,18 @@
 package beubo
 
 import (
+	"archive/zip"
+	"bytes"
 	"flag"
+	"fmt"
+	"github.com/manifoldco/promptui"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 var port = 3000
@@ -13,6 +24,7 @@ func Init() {
 
 // Run runs the main application
 func Run() {
+	checkFiles()
 	settingsInit()
 	databaseInit()
 	databaseSeed()
@@ -24,4 +36,149 @@ func Run() {
 func readCLIFlags() {
 	flag.IntVar(&port, "port", port, "The port you would like the application to listen on")
 	flag.Parse()
+}
+
+// checkFiles checks if there is a theme present and will otherwise prompt to download that theme
+func checkFiles() {
+	_, err := os.Stat("./themes")
+	if os.IsNotExist(err) {
+		if ask("There is no themes folder present, would you like to create it?") {
+			err := os.Mkdir("./themes", 0755)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	if !hasDirectory("./themes/") {
+		if ask("There is no theme installed, would you like to download the default theme?") {
+			resp, err := http.Get("https://github.com/uberswe/beubo-default/archive/master.zip")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Read all the files from zip archive
+			for _, zipFile := range zipReader.File {
+				err := extractAndWriteFile(zipFile)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+			}
+		}
+	}
+	_, err = os.Stat("./themes/install")
+	if os.IsNotExist(err) {
+		if ask("There is no install theme installed, would you like to download the default install theme?") {
+			resp, err := http.Get("https://github.com/uberswe/beubo-install/archive/master.zip")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Read all the files from zip archive
+			for _, zipFile := range zipReader.File {
+				err := extractAndWriteFile(zipFile)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+			}
+		}
+	}
+}
+
+// ask prompts the user for an answer to a question via the command line. Returns true for yes and false for no
+func ask(question string) bool {
+	prompt := promptui.Select{
+		Label: fmt.Sprintf("%s [Yes/No]", question),
+		Items: []string{"Yes", "No"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		return false
+	}
+	if result == "Yes" {
+		return true
+	}
+	return false
+}
+
+// hasDirectory takes a path and checks if a directory exists in the path
+func hasDirectory(path string) bool {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return false
+	}
+	for _, f := range files {
+		log.Println(f.Name(), f.IsDir())
+		if f.IsDir() {
+			return true
+		}
+	}
+	return false
+}
+
+func extractAndWriteFile(f *zip.File) error {
+	dest := "./themes/"
+	rc, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := rc.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	filename := strings.Replace(f.Name, "beubo-default-master/", "default/", 1)
+	filename = strings.Replace(filename, "beubo-install-master/", "install/", 1)
+
+	path := filepath.Join(dest, filename)
+
+	// Check for ZipSlip (Directory traversal)
+	if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
+		return fmt.Errorf("illegal file path: %s", path)
+	}
+
+	if f.FileInfo().IsDir() {
+		os.MkdirAll(path, f.Mode())
+	} else {
+		os.MkdirAll(filepath.Dir(path), f.Mode())
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := f.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		_, err = io.Copy(f, rc)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,27 +3,26 @@ module github.com/uberswe/beubo
 go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.36.18
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/goincremental/negroni-sessions v0.0.0-20171223143234-40b49004abee
-	github.com/golang/gddo v0.0.0-20201222204913-17b648fae295 // indirect
-	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/manifoldco/promptui v0.8.0
+	github.com/mattn/go-colorable v0.0.10-0.20170816031813-ad5389df28cd // indirect
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b
-	github.com/uberswe/beubo_grpc v0.0.0-20201217233052-a073736aeeb2
+	github.com/stretchr/testify v1.5.1 // indirect
 	github.com/urfave/negroni v1.0.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
-	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
-	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
-	google.golang.org/grpc v1.34.0
-	google.golang.org/protobuf v1.25.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.10
